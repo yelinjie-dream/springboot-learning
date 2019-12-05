7612384512a25ccdb60e5671fb8af9b8157c6d9c
yelinjie <yelinjie@huazhu.com>
2019-12-05 23:39:55 +0800

    创建了分支dev

--- springboot-helloworld/src/test/java/org/spring/springboot/web/HttpTest.java
+++ springboot-helloworld/src/test/java/org/spring/springboot/web/HttpTest.java
@@ -8,7 +8,8 @@ package org.spring.springboot.web;
 public class HttpTest {
     public static void main(String[] args) {
         System.out.println("aaaa = " );
-        
+
         System.out.println(" = ");
+        System.out.println("create local dev");
     }
 }
