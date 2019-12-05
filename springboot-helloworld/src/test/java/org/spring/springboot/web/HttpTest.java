0e6487e2ef37ecea13fc91b70eaae4fa7e3b9f3c
yelinjie <yelinjie@huazhu.com>
2019-12-05 23:05:32 +0800

    增加了HtppTest

---
+++ springboot-helloworld/src/test/java/org/spring/springboot/web/HttpTest.java
@@ -0,0 +1,12 @@
+package org.spring.springboot.web;
+
+/**
+ * @author yelinjie
+ * @version 1.0
+ * @date 2019/12/5 23:00
+ */
+public class HttpTest {
+    public static void main(String[] args) {
+        System.out.println("aaaa = " );
+    }
+}
